7ca345a470b6ac9f9f90e53d562f4c88ca0b9435
Jason <jkuffler@catalyte.io>
2022-04-24 21:47:12 -0400

    next - research error loading object datatype

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -6,13 +6,21 @@ const ProfilePage = ({firstName, lastName, data}) => {
 	const InfoDivs = () => (
 		Object.keys(data).map(e => <div className={profilePageStyles.title}>{e+': '} {typeof data[e] === 'object'? JSON.stringify(data[e]) : data[e]}</div>)
 	)
+	console.log(data.location.state)
 
+	const UserBasicInfo = {
+		birthday: data.birthday,
+		email: data.email,
+		location: data.location.state,
+	}
+	
 	return (
-	<>
-		<div className={profilePageStyles.title}>ProfilePage: My name is {firstName+" "+lastName}</div>
-		<InfoDivs/>
-		<Modal> </Modal>
-	</>
+	<div style={{textAlign: 'center'}}>
+		<h1 className={profilePageStyles.title}> Welcome! My name is 	{firstName+" "+lastName}</h1>
+		{/* {<InfoDivs/> } */}
+		{<UserBasicInfo/>}
+		{/* {<Modal> </Modal>} */}
+	</div>
 	)
 }
 
